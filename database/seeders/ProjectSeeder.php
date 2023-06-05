3562a213a34da1f0c192528a47c0f7361cf49f82
StefanoM98 <smartino030@gmail.com>
2023-06-05 14:47:24 +0200

    Add: Migrate table in db

---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Project;
+use Illuminate\Support\Str;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $newProject = new Project();
+            $newProject->title = $faker->sentence(4);
+            $newProject->slug = Str::slug($newProject->title, '%');
+            $newProject->description = $faker->text();
+            $newProject->save();
+        }
+    }
+}
